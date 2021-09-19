7f226ea6663b39f9acb3c5123c7225f4af0992e8
amilaperera97 <amilaperera97@gmail.com>
2021-09-19 23:43:47 +0100

    Project Initial

---
+++ src/main/java/com/trains/steveTrains/SteveTrainsApplication.java
@@ -0,0 +1,13 @@
+package com.trains.steveTrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteveTrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteveTrainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trains/steveTrains/SteveTrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trains.steveTrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteveTrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
